18006ea5fac94da4bf42abbedb446ed654df73ec
ivankristianto <ivankristianto@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-05 15:31:37 +0000

    push tag 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/js-loader/tags/1.0.1@1213552 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js_loader.php
+++ js_loader.php
@@ -3,7 +3,7 @@
  * Plugin Name: JavaScript Loader (CDN Edition)
  * Plugin URI: https://github.com/calibreworks/js_loader
  * Description: JavaScript Loader CDN Edition is a plugin to download various JavaScript Plugin directly from jsdelivr.com
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Ivan Kristianto, Ade Viankakrisna Fadlil
  * Author URI: https://www.ivankristianto.com, http://www.viankakrisna.com
  * Requires at least: 4.1
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 define( 'JSLOADER__MINIMUM_WP_VERSION', '4.1' );
-define( 'JSLOADER__VERSION',            '1.0.0' );
+define( 'JSLOADER__VERSION',            '1.0.1' );
 define( 'JSLOADER__PLUGIN_DIR',         plugin_dir_path( __FILE__ ) );
 define( 'JSLOADER__PLUGIN_URL',         plugins_url( '', __FILE__ ) );
 define( 'JSLOADER__PLUGIN_FILE',        __FILE__ );
